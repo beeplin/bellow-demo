408f82880dfb9a94a0eba39d7d4eb449fd01be4d
beeplin <beeplin@gmail.com>
2021-04-18 13:41:17 +0800

    0.5.0

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import cors from 'cors'
 import express, { Request, Response } from 'express'
 import path from 'path'
-import telecall from 'telecall/lib/express-middleware'
+import telecall from 'telecall/lib/express'
 
 export interface Context {
   req: Request
@@ -12,10 +12,20 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(telecall(app))
+
+app.use(
+  telecall.path,
+  telecall.middleware({
+    convertResolverPath: (original) =>
+      original.endsWith('.ts')
+        ? original.replace(/^\/src\/(.*?)\.ts$/, '/dist/$1.js')
+        : original,
+  }),
+)
+
 app.use(express.static(path.join(__dirname, '../../www/dist')))
 
-const port = app.get('port')
+const port = telecall.port
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
